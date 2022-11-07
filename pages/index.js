5ccd81fb76856d8d4ba2a7c2444229a10310a948
ABSanthosh <a.b.santhosh02@gmail.com>
2022-11-07 22:20:07 +0530

    Project Init

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import "../styles/routes/Home.scss";
+
+export default function Home() {
+  return (
+    <div className="LandingPage">
+      New ECell Site<a href="#login">login</a>
+    </div>
+  );
+}
